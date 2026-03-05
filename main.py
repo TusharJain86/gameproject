a9e68e93fc2aa8dc767d5dd363ef263c27d7b01a
TusharJain86 <tusharjain0806@gmail.com>
2026-03-06 01:54:30 +0530

    random import

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-computer= -1
-youstr= input("Enter your choice: ")
+import random
 
-youDict= {
-    "s":1,
-    "w":-1,
-    "g":0
+computer = -1
+youstr = input("Enter your choice: ")
+
+youDict = {
+    "s": 1,
+    "w": -1,
+    "g": 0
 }
 
 you = youDict[youstr]
+reverseDict= { 1: "Snake", -1: "Water", 0: "Gun" }
 
-if(computer == -1 and you ==1):
-    print("You win!")
+print(f" you chose {dict[you]} \n computer chose {reverseDict[computer]}")
 
-elif(computer == -1 and you == 0):
-    print("Computer wins! Try again.")
+if computer == you:
+    print("It's a tie! Try again.")
 
-elif(computer == 1 and you == -1):
-    print("Computer wins! Try again.")
+else:
+    if computer == -1 and you == 1:
+        print("You win!")
 
-elif(computer == 1 and you == 0):
-    print("You win!")
+    elif computer == -1 and you == 0:
+        print("Computer wins! Try again.")
 
-elif(computer == 0 and you == -1):
-    print("Computer wins! Try again.")
+    elif computer == 1 and you == -1:
+        print("Computer wins! Try again.")
 
-elif(computer == 0 and you == 1):
-    print("You win!")
+    elif computer == 1 and you == 0:
+        print("You win!")
 
-else:    print("It's a tie! Try again or something went wrong.")
+    elif computer == 0 and you == -1:
+        print("Computer wins! Try again.")
+
+    elif computer == 0 and you == 1:
+        print("You win!")
+
+    else:
+        print("Something went wrong.")
